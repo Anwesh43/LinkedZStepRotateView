16cbb2c32ca205abc5019624b95259dc27968d0d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-16 11:26:17 +0530

    importing libraries and defining nodes

--- zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
+++ zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
@@ -3,3 +3,12 @@ package ui.anwesome.com.zsteprotateview
 /**
  * Created by anweshmishra on 16/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
