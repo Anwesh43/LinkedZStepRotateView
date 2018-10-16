fb40696537e4eb5e143407164668c22205e88228
Anwesh43 <anweshthecool0@gmail.com>
2018-10-16 12:10:11 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/linkedzsteprotateview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkedzsteprotateview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.linkedzsteprotateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.zsteprotateview.ZStepRotateView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ZStepRotateView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
