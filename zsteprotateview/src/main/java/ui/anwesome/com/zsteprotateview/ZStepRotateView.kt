21d642fc416ce7cadd8dccb367602bec15785b9d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-16 11:55:54 +0530

    adding ZStepRotate class

--- zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
+++ zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
@@ -143,7 +143,32 @@ class ZStepRotateView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class ZStepRotate(var i : Int) {
+        private val root : ZSRNode = ZSRNode(0)
+
+        private var curr : ZSRNode = root
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
