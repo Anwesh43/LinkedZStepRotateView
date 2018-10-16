049cc1c1180145a4ddeacf724c29124eb9853709
Anwesh43 <anweshthecool0@gmail.com>
2018-10-16 11:36:31 +0530

    adding Animator class

--- zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
+++ zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
@@ -49,4 +49,33 @@ class ZStepRotateView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
