b17e9c6bfe97a09260dbfea4307e9a73895cfbf7
Anwesh43 <anweshthecool0@gmail.com>
2018-10-16 11:28:14 +0530

    adding ZStepRotateView class

--- zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
+++ zsteprotateview/src/main/java/ui/anwesome/com/zsteprotateview/ZStepRotateView.kt
@@ -12,3 +12,21 @@ import android.graphics.Color
 import android.content.Context
 
 val nodes : Int = 5
+
+class ZStepRotateView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
